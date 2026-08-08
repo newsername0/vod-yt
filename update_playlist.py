681d25247dbc8ac249f240a279ddcd0f3373f242
Ian Mazurik <mazurikian@protonmail.com>
2026-08-08 17:31:11 -0300

    Add files via upload

---
+++ update_playlist.py
@@ -0,0 +1,21 @@
+import requests
+
+INPUT_FILE = "playlist_source.m3u"
+OUTPUT_FILE = "playlist.m3u"
+
+FIRST_PREFIX = "https://chromecast.cvattv.com.ar/live/"
+SECOND_PREFIX = "https://cdn.cvattv.com.ar/live/"
+
+with (
+    open(INPUT_FILE, "r", encoding="utf-8") as input_file,
+    open(OUTPUT_FILE, "w", encoding="utf-8") as output_file,
+):
+    for line in input_file:
+        url = line.rstrip("\n")
+        if url.startswith((FIRST_PREFIX, SECOND_PREFIX)):
+            response = requests.head(url, allow_redirects=False)
+            if "Location" in response.headers:
+                url = response.headers["Location"]
+        output_file.write(url + "\n")
+
+print("Done")
